362c7c9747998111047ff7545497f6ebe7ea6ff0
João Otávio <joaossousaa31@gmail.com>
2024-04-19 15:09:33 -0300

    media simples calculada2

---
+++ Functions/mediaSimples.js
@@ -0,0 +1,4 @@
+export default function mediaSimples(...numbers) {
+    const sum = numbers.reduce((acc, num) => acc + num, 0)
+    return sum / numbers.length
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+import  mediaSimples  from "./Functions/mediaSimples.js"
+
+console.log(mediaSimples(2, 6, 3, 7, 4))
