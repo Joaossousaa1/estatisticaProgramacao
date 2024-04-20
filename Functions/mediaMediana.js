57a82b303b18eb219b9fe09716b62c419cbf517f
João note <joaossousaa31@gmail.com>
2024-04-19 21:47:51 -0300

    Media mediana feita

--- Functions/mediaMediana.js
+++ Functions/mediaMediana.js
@@ -1,3 +1,12 @@
-export default function mediaMediana() {
-    console.log(`testeNovamente`)
+export default function mediaMediana(mediaSimples, ...numbers) {
+    if (numbers.length % 2 === 0) {
+        let num = {a: (numbers.length / 2) - 1, b: numbers.length / 2}
+        return mediaSimples(numbers[num.a], numbers[num.b])
+    } else {
+        while (numbers.length > 1) {
+            numbers.pop()
+            numbers.shift()
+        }
+        return numbers[0]
+    }
 }
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import  mediaSimples  from "./Functions/mediaSimples.js"
+import mediaSimples  from "./Functions/mediaSimples.js"
 import mediaPonderada from "./Functions/mediaPonderada.js"
+import mediaMediana from "./Functions/mediaMediana.js"
 
 console.log(mediaSimples(2, 6, 3, 7, 4))
 console.log(mediaPonderada({ n: 7, p: 2}, { n: 9, p: 5 }, { n: 3, p: 1 }))
+console.log(mediaMediana(mediaSimples, 15, 14, 8, 7, 3))
